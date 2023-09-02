d52ff39d3106b9a7cb0247db37304644a414dad6
Mohamed TCHOKETCH <mohamedtchoketch0@gmail.com>
2023-09-03 01:45:13 +0200

    Completed assignment

--- index.js
+++ index.js
@@ -8,3 +8,6 @@ const newHeader = document.createElement('h1');
 newHeader.id = 'victory';
 
 newHeader.textContent = 'Mohamed TCHOKETCH is the champion';
+
+// Appending the element to the DOM for it to be displayed on the web page
+document.body.appendChild(newHeader);
